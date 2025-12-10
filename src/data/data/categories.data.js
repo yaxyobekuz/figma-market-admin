8746340bc06fc61d1accdb0520d847ab051e745c
yaxyobekuz <pubgn9642@gmail.com>
2025-12-10 19:19:33 +0500

    add categories data for exploration links

---
+++ src/data/data/categories.data.js
@@ -0,0 +1,39 @@
+const categories = [
+  {
+    name: "All",
+    slug: "all",
+    href: "/explore/all",
+  },
+  {
+    name: "Web Site",
+    slug: "web-site",
+    href: "/explore/web-site",
+  },
+  {
+    name: "Mobile App",
+    slug: "mobile-app",
+    href: "/explore/mobile-app",
+  },
+  {
+    name: "UI Kit",
+    slug: "ui-kit",
+    href: "/explore/ui-kit",
+  },
+  {
+    name: "Icon Set",
+    slug: "icon-set",
+    href: "/explore/icon-set",
+  },
+  {
+    name: "Illustration",
+    slug: "illustration",
+    href: "/explore/illustration",
+  },
+  {
+    name: "3D Asset",
+    slug: "3d-asset",
+    href: "/explore/3d-asset",
+  },
+];
+
+export default categories;
